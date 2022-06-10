f363064367cb2051dd0e57c24f1945dbe79b0190
josevalpo <jose.valpo@gmail.com>
2022-06-09 23:02:24 -0400

    Actualización 6
    CRUD inicial

--- TestDjango/core/admin.py
+++ TestDjango/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categoria, Producto
+
+admin.site.register(Categoria)
+admin.site.register(Producto)
+
 
 # Register your models here.
--- TestDjango/core/models.py
+++ TestDjango/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(
+        primary_key=True, verbose_name='Id de categoria')
+    nombreCategoria = models.CharField(
+        max_length=50, verbose_name="Nombre de la categoria")
+
+    def _str_(self):
+        return self.nombreCategoria
+
+
+class Producto(models.Model):
+    sku = models.CharField(max_length=6, primary_key=True, verbose_name='Sku')
+    nombre = models.CharField(max_length=20, verbose_name='Nombre')
+    precio = models.CharField(max_length=20, null=True,
+                              blank=True, verbose_name='precio')
+    stock = models.CharField(max_length=20, null=True,
+                             blank=True, verbose_name='stock')
+    descripcion = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name='descripcion')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.sku
