8be287a170e25d8bba13b3931288b5fd0a95eb74
sobarzolicandeo <sobarzolicandeo@gmail.com>
2022-06-10 21:29:08 -0400

    Actualizacion 12
    Modificacion views

--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -6,29 +6,38 @@ from .forms import ProductoForm
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'core/index.html')
+
 
 def index2(request):
     return render(request, 'core/index.html')
 
+
 def bandanas(request):
     return render(request, 'core/bandanas.html')
+
 
 def correas(request):
     return render(request, 'core/correas.html')
 
+
 def identificadores(request):
     return render(request, 'core/identificadores.html')
+
 
 def bandana_producto_1(request):
     return render(request, 'core/bandana_producto_1.html')
 
+
 def correa_producto_1(request):
     return render(request, 'core/correa_producto_1.html')
 
+
 def identificador_producto_1(request):
     return render(request, 'core/identificador_producto_1.html')
+
 
 def registro(request):
     return render(request, 'core/registro.html')
@@ -36,12 +45,14 @@ def registro(request):
 # def productos(request):
 #     return render(request, 'core/productos.html')
 
+
 def productos(request):
     datos = {}
     return render(request, 'core/productos.html', datos)
 
 # def form_producto(request):
 #     return render(request, 'core/form_producto.html')
+
 
 def form_producto(request):
     datos = {
@@ -58,6 +69,7 @@ def form_producto(request):
 # def form_mod_producto(request):
 #     return render(request, 'core/form_mod_producto.html')
 
+
 def form_mod_producto(request, id):
     producto = Producto.objects.get(sku=id)
     datos = {
@@ -72,6 +84,7 @@ def form_mod_producto(request, id):
 
 # def form_del_producto(request):
 #     return render(request, 'core/form_del_producto.html')
+
 
 def form_del_producto(request, id):
     producto = Producto.objects.get(sku=id)
