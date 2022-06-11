7ef823cad45174aa02ac7ccd4cecebaade3e3da4
josevalpo <jose.valpo@gmail.com>
2022-06-10 21:23:59 -0400

    Actualización 11
    Avance CRUD

--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -1,5 +1,8 @@
 from turtle import home
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template import loader
+from .models import Producto
+from .forms import ProductoForm
 
 # Create your views here.
 
@@ -30,14 +33,47 @@ def identificador_producto_1(request):
 def registro(request):
     return render(request, 'core/registro.html')
 
+# def productos(request):
+#     return render(request, 'core/productos.html')
+
 def productos(request):
-    return render(request, 'core/productos.html')
+    datos = {}
+    return render(request, 'core/productos.html', datos)
+
+# def form_producto(request):
+#     return render(request, 'core/form_producto.html')
 
 def form_producto(request):
-    return render(request, 'core/form_producto.html')
+    datos = {
+        'form': ProductoForm()
+    }
+    if request.method == 'POST':
+        formulario = ProductoForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Datos guardados correctamente"
 
-def form_mod_producto(request):
-    return render(request, 'core/form_mod_producto.html')
+    return render(request, 'core/form_producto.html', datos)
 
-def form_del_producto(request):
-    return render(request, 'core/form_del_producto.html')
+# def form_mod_producto(request):
+#     return render(request, 'core/form_mod_producto.html')
+
+def form_mod_producto(request, id):
+    producto = Producto.objects.get(sku=id)
+    datos = {
+        'form': ProductoForm(instance=producto)
+    }
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, instance=producto)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Datos modificados correctamente"
+    return render(request, 'core/form_mod_producto.html', datos)
+
+# def form_del_producto(request):
+#     return render(request, 'core/form_del_producto.html')
+
+def form_del_producto(request, id):
+    producto = Producto.objects.get(sku=id)
+    producto.delete()
+    return redirect(to="productos")
